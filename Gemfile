5d1ef11219f55c605d851dad2e601d7ca28e5c87
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-01 16:56:36 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source :rubygems
+ruby '1.9.3'
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
+gem 'sinatra'
+gem 'sinatra-contrib'
+
+# Use Thin for our web server
+gem 'thin'
+gem 'bcrypt-ruby', '~>3.0.0'
+gem 'activesupport'
+gem 'activerecord'
+gem 'mail'
+gem 'twitter'
+gem 'googlecharts'
+gem 'heroku'
+gem 'rake'
+gem 'sinatra-flash'
+
+gem 'shotgun'
+gem 'faker'
+
+group :test do
+  gem 'rspec'
+end
---
+++ public/js/application.js
@@ -0,0 +1,71 @@
+// $(document).ready(function() {
+
+//   if($('#survey-title').text() === "New Survey"){
+
+//     $('#add-question-btn').hide();
+//     $('.survey-nav-buttons').hide();
+//   }
+
+//   $('#edit-title-form').submit(function(updateTitle){
+//     updateTitle.preventDefault();
+   
+//     var url = $(this).attr('action');
+//     var data = $(this).serialize();
+
+//     $.post(url, data, function(response){
+//       $('#survey-title').html($(response).find('#survey-title').text());
+//     });
+  
+//     $('#question-textarea').show();
+//     $('#add-question-btn').show();
+//     $('.survey-nav-buttons').show();
+//   });
+
+//   $('#add-question-form').submit(function(addQuestion){
+//     addQuestion.preventDefault();
+
+//     var url = $(this).attr('action');
+//     var data = $(this).serialize();
+    
+//     $.post(url, data, function(response){
+//       console.log(response);
+//       $('.question-container').html($(response).find('.question-container'));
+
+//       $('#question-textarea').val("");
+//       $('.survey-nav-buttons').show();
+//     });
+//   });
+
+//   $('.new-response-form').submit(function(addQuestion){
+//     addQuestion.preventDefault();
+
+//       var url = $(this).attr('action');
+//       var data = $(this).serialize();
+
+//       $.post(url, data, function(response){
+//         console.log(response);
+//         $('.responses-container').html($(response).find('.responses-container'));
+//         $('.delete-response-form btn').show();
+//       });
+//   });
+
+//   $('.save-response-form').submit(function(saveResponse){
+//     saveResponse.preventDefault();
+
+//     var url = $(this).attr('action');
+//     var data = $(this).serialize();
+
+//     $.post(url, data)
+//   });
+
+//   $('.delete-response-form').submit(function(deleteResponse){
+//     deleteResponse.preventDefault();
+
+//     var url = $(this).attr('action');
+//     var data = $(this).serialize();
+//     $.post(url, data)
+//     $(this).parent().hide();
+//   });
+
+
+// });
